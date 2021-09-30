cb05efcea662928b75e886eba0ae71d65ad113cb
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2021-09-30 10:58:04 +0530

    give much more control to the user

--- config.h
+++ config.h
@@ -1,95 +1,34 @@
-static const unsigned short int left = 7;      // no. of left aligned blocks
-static const unsigned short int centre = 0;    // no. of centre aligned blocks
+/* Lemonaid does not do lemonbar specific formatting for you
+ * This means you yourself have to add the format specifiers for bg, fg,
+ * underline, overline, etc.
+ * This gives the user much more control over how they want to setup their bar.
+ * Lemonaid only handles the signals and runs your commands after the specified
+ * time interval.
+ * This makes lemonaid more akin to the unix philosophy and helps make the
+ * codebase lean and clean
+ */
+
+static const unsigned short int left = 7;     // no. of left aligned blocks
+static const unsigned short int centre = 0;   // no. of centre aligned blocks
 static const unsigned short int right = 1;    // no. of right aligned blocks
 
 static const Block blocks[] = {
-    {
-        "cpu",                  // command
-        10,                     // update interval
-        1,                      // update signal
-        NULL,                   // bg color
-        NULL,                   // fg color
-        true,                   // underline
-        false,                  // overline
-        NULL                    // underline or overline color
-    },
+    // command                  update interval(in s)   update signal
+    { "cpu",                    10,                     1 },
 
-    {
-        "mem",
-        60,
-        2,
-        NULL,
-        NULL,
-        true,
-        false,
-        NULL
-    },
+    { "mem",                    60,                     2 },
 
-    {
-        "cpu-tempt",
-        10,
-        3,
-        NULL,
-        NULL,
-        true,
-        false,
-        NULL
-    },
+    { "cpu-tempt",              10,                     3 },
 
-    {
-        "vol",
-        0,
-        4,
-        NULL,
-        NULL,
-        true,
-        false,
-        NULL
-    },
+    { "vol",                    0,                      4 },
 
-    {
-        "brightness-indicator",
-        0,
-        5,
-        NULL,
-        NULL,
-        true,
-        false,
-        NULL
-    },
+    { "brightness-indicator",   0,                      5 },
 
-    {
-        "battery",
-        60,
-        6,
-        NULL,
-        NULL,
-        true,
-        false,
-        NULL
-    },
+    { "battery",                60,                     6 },
 
-    {
-        "wifi-indicator",
-        0,
-        7,
-        NULL,
-        NULL,
-        true,
-        false,
-        NULL
-    },
+    { "wifi-indicator",         0,                      7 },
 
-    {
-        "clock",
-        60,
-        8,
-        NULL,
-        NULL,
-        true,
-        false,
-        NULL
-    },
+    { "clock",                  60,                     8 },
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
--- lemonaid.c
+++ lemonaid.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define CMDLEN 70
+#define CMDLEN 100
 #define LEN(X) sizeof(X)/sizeof(X[0])
 #define FORBLOCKINBLOCKS for (unsigned int i = 0; i < LEN(blocks); i ++)
 
@@ -12,38 +12,26 @@ typedef struct {
     char *cmd;
     int interval;
     int signal;
-    char *bg_color;
-    char *fg_color;
-    bool underline;
-    bool overline;
-    char *underline_color;
 } Block;
 
 #include "config.h"
 
 static char statusbar[LEN(blocks)][CMDLEN] = {0};
+
 static void sighandler(int signal);
 static void update(unsigned long long int time);
 static void run_cmd(unsigned int index, const Block *block, char block_text[]);
-static void setup_sighandlers();
 static void write_status();
 
-static void bg(char *bg_color, char open_bg[], char close_bg[]);
-static void fg(char *fg_color, char open_fg[], char close_fg[]);
-static void line(bool underline, bool overline, char *underline_color, char open_line[], char close_line[]);
-
 int main() {
-    setup_sighandlers();
+    FORBLOCKINBLOCKS signal(SIGRTMIN + blocks[i].signal, sighandler);
     unsigned long int time = 0;
     while (1) {
         update(time);
         time ++;
         sleep(1.0);
     }
-}
-
-void setup_sighandlers() {
-    FORBLOCKINBLOCKS signal(SIGRTMIN + blocks[i].signal, sighandler);
+    return 0;
 }
 
 void sighandler(int signal) {
@@ -62,43 +50,34 @@ void run_cmd(unsigned int index, const Block *block, char block_text[]) {
     FILE *cmdf = popen(block -> cmd, "r");
     if (!cmdf) return;
 
-    char buffer[CMDLEN - delim_length];
-
     // Get output of the command
-    fgets(buffer, CMDLEN - delim_length, cmdf);
+    fgets(block_text, CMDLEN - delim_length, cmdf);
 
-    // remove newline char
-    unsigned int len = strlen(buffer);
-    if (buffer[len - 1] == '\n') buffer[len - 1] = '\0';
+    // close
+    pclose(cmdf);
 
-    char open_bg[15], close_bg[15], open_fg[15], close_fg[15], open_line[25], close_line[25];
-    bg(block -> bg_color, open_bg, close_bg);
-    fg(block -> fg_color, open_fg, close_fg);
-    line(block -> underline, block -> overline, block -> underline_color, open_line, close_line);
-
-    snprintf(
-        block_text,
-        CMDLEN - delim_length - 1,
-        "%s%s%s %s %s%s%s",
-        open_bg,
-        open_fg,
-        open_line,
-        buffer,
-        close_line,
-        close_fg,
-        close_bg
-    );
+    // remove newline char at the end
+    unsigned int len = strlen(block_text);
+    if (block_text[len - 1] == '\n') block_text[len - 1] = '\0';
 
     // add the delimeter if block is not the last one
-    if (index != LEN(blocks) - 1) strncat(block_text, delim, CMDLEN - strlen(block_text) - 1);
+    if (index != LEN(blocks) - 1) strncat(block_text, delim, CMDLEN - len - 1);
+}
 
-    pclose(cmdf);
+void update(unsigned long long int time) {
+    bool write = false;
+    FORBLOCKINBLOCKS {
+        if ((blocks[i].interval != 0 && time % blocks[i].interval == 0) || time == 0) {
+            run_cmd(i, &blocks[i], statusbar[i]);
+            write = true;
+        }
+    }
+    if (write) write_status();
 }
 
 void write_status() {
     unsigned int len =  CMDLEN * LEN(blocks);
     char buffer[len];
-
     unsigned int i = 0;
 
     strncpy(buffer, "%{l}", len - 1);
@@ -115,67 +94,4 @@ void write_status() {
 
     fprintf(stdout, "%s", buffer);
     fflush(stdout);
-}
-
-void update(unsigned long long int time) {
-    bool write = false;
-    FORBLOCKINBLOCKS {
-        if ((blocks[i].interval != 0 && time % blocks[i].interval == 0) || time == 0) {
-            run_cmd(i, &blocks[i], statusbar[i]);
-            write = true;
-        }
-    }
-    if (write) write_status();
-}
-
-void bg(char *bg_color, char open_bg[], char close_bg[]) {
-    if (bg_color != NULL) {
-        snprintf(open_bg, 15, "%%{B%s}", bg_color);
-        snprintf(close_bg, 15, "%%{B}");
-    } else {
-        snprintf(open_bg, 2, "");
-        snprintf(close_bg, 2, "");
-    }
-}
-
-void fg(char *fg_color, char open_fg[], char close_fg[]) {
-    if (fg_color != NULL) {
-        snprintf(open_fg, 15, "%%{F%s}", fg_color);
-        snprintf(close_fg, 15, "%%{F}");
-    } else {
-        snprintf(open_fg, 2, "");
-        snprintf(close_fg, 2, "");
-    }
-}
-
-// Look at this mess. TODO: refactor
-void line(bool underline, bool overline, char *underline_color, char open_line[], char close_line[]) {
-    if (underline && overline) {
-        if (underline_color != NULL) {
-            snprintf(open_line, 25, "%%{U%s}%%{+u}%%{+o}", underline_color);
-            snprintf(close_line, 25, "%%{-o}%%{-u}%%{U}");
-        } else {
-            snprintf(open_line, 25, "%%{+u}%%{+o}");
-            snprintf(close_line, 25, "%%{-o}%%{-u}");
-        }
-    } else if (underline) {
-        if (underline_color != NULL) {
-            snprintf(open_line, 25, "%%{U%s}%%{+u}", underline_color);
-            snprintf(close_line, 25, "%%{-u}%%{U}");
-        } else {
-            snprintf(open_line, 25, "%%{+u}");
-            snprintf(close_line, 25, "%%{-u}");
-        }
-    } else if (overline) {
-        if (underline_color != NULL) {
-            snprintf(open_line, 25, "%%{U%s}%%{+o}", underline_color);
-            snprintf(close_line, 25, "%%{-o}%%{U}");
-        } else {
-            snprintf(open_line, 25, "%%{+o}");
-            snprintf(close_line, 25, "%%{-o}");
-        }
-    } else {
-            snprintf(open_line, 2, "");
-            snprintf(close_line, 2, "");
-    }
 }
